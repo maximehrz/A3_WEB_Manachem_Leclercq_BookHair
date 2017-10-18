7fa1770463cfd63979b3903050f8cc241277b08b
Unknown <titouan.manachem@gmail.com>
2017-10-18 17:59:00 +0200

    Auth make

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Route::resource('/coiffeur', 'CoiffeurController' );
 Route::resource('/rdv', 'RdvController' );
 
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
